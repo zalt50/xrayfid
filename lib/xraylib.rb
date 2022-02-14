9caa9610aacc9fd61afbad6061b6b42173329bf1
Takahiro SATOH <zalt50cc@gmail.com>
2022-02-14 23:44:18 +0900

    Add atomic_weight, element_density and cs
    Signed-off-by: Takahiro SATOH <zalt50cc@gmail.com>

--- lib/xraylib.rb
+++ lib/xraylib.rb
@@ -1,8 +1,45 @@
 # frozen_string_literal: true
 
 require_relative "xraylib/version"
+require "fiddle/import"
 
 module Xraylib
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Libxrl
+    extend Fiddle::Importer
+
+    lib_name = "libxrl.#{RbConfig::CONFIG["SOEXT"]}"
+    begin
+      dlload lib_name
+    rescue Fiddle::DLError
+      dlload "#{ENV["HOMEBREW_PREFIX"]}/lib/#{lib_name}" if ENV["HOMEBREW_PREFIX"]
+    end
+
+    # Atomic weights
+    extern "double AtomicWeight(int Z, xrl_error **error);"
+
+    # Element densities
+    extern "double ElementDensity(int Z, xrl_error **error);"
+
+    # Cross sections
+    extern "double CS_Total(int Z, double E, xrl_error **error);"
+    extern "double CS_Photo(int Z, double E, xrl_error **error);"
+    extern "double CS_Rayl(int Z, double E, xrl_error **error);"
+    extern "double CS_Compt(int Z, double E, xrl_error **error);"
+  end
+
+  module_function
+
+  # Atomic weights
+  def atomic_weight(z) = Libxrl.AtomicWeight(z, nil)
+
+  # Element densities
+  def element_density(z) = Libxrl.ElementDensity(z, nil)
+
+  # Cross sections
+  def cs_total(z, energy) = Libxrl.CS_Total(z, energy, nil)
+  def cs_photo(z, energy) = Libxrl.CS_Photo(z, energy, nil)
+  def cs_rayl(z, energy) = Libxrl.CS_Rayl(z, energy, nil)
+  def cs_compt(z, energy) = Libxrl.CS_Compt(z, energy, nil)
 end
