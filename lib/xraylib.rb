111d4d898a47cc9b80af9c12092b518ba29d2e0d
Takahiro SATOH <zalt50cc@gmail.com>
2022-02-14 22:05:21 +0900

    Initial commit
    Signed-off-by: Takahiro SATOH <zalt50cc@gmail.com>

---
+++ lib/xraylib.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "xraylib/version"
+
+module Xraylib
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/xraylib/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Xraylib
+  VERSION = "0.1.0"
+end
