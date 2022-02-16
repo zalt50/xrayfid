064e23d8dcdc0e4cd8efad1fa06c6bae87ab2c22
Takahiro SATOH <zalt50cc@gmail.com>
2022-02-16 16:40:00 +0900

    Collect definitions
    Signed-off-by: Takahiro SATOH <zalt50cc@gmail.com>

--- lib/xraylib.rb
+++ lib/xraylib.rb
@@ -18,32 +18,6 @@ module Xraylib
     rescue Fiddle::DLError
       dlload "#{ENV["HOMEBREW_PREFIX"]}/lib/#{lib_name}" if ENV["HOMEBREW_PREFIX"]
     end
-
-    # Atomic weights
-    extern "double AtomicWeight(int Z, xrl_error **error);"
-
-    # Element densities
-    extern "double ElementDensity(int Z, xrl_error **error);"
-
-    # Cross sections
-    %w[Total Photo Rayl Compt].each do |name|
-      extern "double CS_#{name}(int Z, double E, xrl_error **error);"
-      extern "double CSb_#{name}(int Z, double E, xrl_error **error);"
-    end
-
-    %w[FluorLine FluorShell].each do |name|
-      extern "double CS_#{name}(int Z, int line, double E, xrl_error **error);"
-      extern "double CSb_#{name}(int Z, int line, double E, xrl_error **error);"
-    end
-
-    %w[Rayl Compt].each do |name|
-      extern "double DCS_#{name}(int Z, double E, double theta, xrl_error **error);"
-      extern "double DCSb_#{name}(int Z, double E, double theta, xrl_error **error);"
-      extern "double DCSP_#{name}(int Z, double E, double theta, double phi, xrl_error **error);"
-      extern "double DCSPb_#{name}(int Z, double E, double theta, double phi, xrl_error **error);"
-    end
-
-    extern "double CS_Energy(int z, double E, xrl_error **error);"
   end
 
   AVOGNUM = 0.602214129   # Avogadro number (mol-1 * barn-1 * cm2)
@@ -55,44 +29,44 @@ module Xraylib
   module_function
 
   # Atomic weights
+  Libxrl.extern "double AtomicWeight(int Z, xrl_error **error);"
   def atomic_weight(z) = Libxrl.AtomicWeight(z, nil)
 
   # Element densities
+  Libxrl.extern "double ElementDensity(int Z, xrl_error **error);"
   def element_density(z) = Libxrl.ElementDensity(z, nil)
 
   # Cross sections
-  %w[Total Photo Rayl Compt].each do |name|
-    define_method("cs_#{name.downcase}") do |z, energy|
-      Libxrl.send("CS_#{name}", z, energy, nil)
+  %w[CS CSb].each do |prefix|
+    %w[Total Photo Rayl Compt].each do |name|
+      Libxrl.extern "double #{prefix}_#{name}(int Z, double E, xrl_error **error);"
+      define_method("#{prefix}_#{name}".downcase) do |z, energy|
+        Libxrl.send("#{prefix}_#{name}", z, energy, nil)
+      end
     end
-    define_method("csb_#{name.downcase}") do |z, energy|
-      Libxrl.send("CSb_#{name}", z, energy, nil)
-    end
-  end
-
-  %w[FluorLine FluorShell].each do |name|
-    define_method("cs_#{name.underscore}") do |z, line, energy|
-      Libxrl.send("CS_#{name}", z, line, energy, nil)
-    end
-    define_method("csb_#{name.underscore}") do |z, line, energy|
-      Libxrl.send("CSb_#{name}", z, line, energy, nil)
+    %w[FluorLine FluorShell].each do |name|
+      Libxrl.extern "double #{prefix}_#{name}(int Z, int line, double E, xrl_error **error);"
+      define_method("#{prefix.downcase}_#{name.underscore}") do |z, line, energy|
+        Libxrl.send("#{prefix}_#{name}", z, line, energy, nil)
+      end
     end
   end
 
   %w[Rayl Compt].each do |name|
-    define_method("dcs_#{name.downcase}") do |z, energy, theta|
-      Libxrl.send("DCS_#{name}", z, energy, theta, nil)
+    %w[DCS DCSb].each do |prefix|
+      Libxrl.extern "double #{prefix}_#{name}(int Z, double E, double theta, xrl_error **error);"
+      define_method("#{prefix}_#{name}".downcase) do |z, energy, theta|
+        Libxrl.send("#{prefix}_#{name}", z, energy, theta, nil)
+      end
     end
-    define_method("dcsb_#{name.downcase}") do |z, energy, theta|
-      Libxrl.send("DCSb_#{name}", z, energy, theta, nil)
-    end
-    define_method("dcsp_#{name.downcase}") do |z, energy, theta, phi|
-      Libxrl.send("DCSP_#{name}", z, energy, theta, phi, nil)
-    end
-    define_method("dcspb_#{name.downcase}") do |z, energy, theta, phi|
-      Libxrl.send("DCSPb_#{name}", z, energy, theta, phi, nil)
+    %w[DCSP DCSPb].each do |prefix|
+      Libxrl.extern "double #{prefix}_#{name}(int Z, double E, double theta, double phi, xrl_error **error);"
+      define_method("#{prefix}_#{name}".downcase) do |z, energy, theta, phi|
+        Libxrl.send("#{prefix}_#{name}", z, energy, theta, phi, nil)
+      end
     end
   end
 
+  Libxrl.extern "double CS_Energy(int z, double E, xrl_error **error);"
   def cs_energy(z, energy) = Libxrl.CS_Energy(z, energy, nil)
 end
