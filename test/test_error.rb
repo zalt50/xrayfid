f3e258c2ecca4e887449cd81e9040b3970e5cc91
Takahiro SATOH <zalt50cc@gmail.com>
2022-02-19 14:41:13 +0900

    Amend test_error
    Signed-off-by: Takahiro SATOH <zalt50cc@gmail.com>

--- test/test_error.rb
+++ test/test_error.rb
@@ -6,9 +6,12 @@ class TestXrayfid < Minitest::Test
   include Xrayfid
 
   def test_error_code
-    error = Fiddle::Pointer.malloc(Fiddle::SIZEOF_INTPTR_T)
-    Libxrl.AtomicWeight(20, error)
-    error.ptr[0, Libxrl::SIZEOF_ERRORCODE].unpack("I!") unless error.ptr.null?
-    # assert_equal 1, *error.ptr[0, Libxrl::SIZEOF_ERRORCODE].unpack("I!")
+    error = Fiddle::Pointer.malloc(Fiddle::SIZEOF_INTPTR_T, Fiddle::RUBY_FREE)
+    Libxrl.AtomicWeight(26, error)
+    assert error.ptr.null?
+    Libxrl.xrl_error_free(error.ptr)
+    Libxrl.AtomicWeight(185, error)
+    assert_equal Libxrl::ERROR_INVALID_ARGUMENT, error.ptr[0, Libxrl::SIZEOF_ERRORCODE].unpack1("I!")
+    Libxrl.xrl_error_free(error.ptr)
   end
 end
